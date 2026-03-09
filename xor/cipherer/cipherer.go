8884509c3bdc18354ebb3bedef54db2bb3493054
Hryhorii Boiko <hryhoriiboiko19@gmail.com>
2026-03-09 10:02:56 +0200

    finish 2nd lesson

---
+++ xor/cipherer/cipherer.go
@@ -0,0 +1,56 @@
+package cipherer
+
+import (
+	"encoding/base64"
+	"errors"
+)
+
+func Cipher(rawString, secret string) (string, error) {
+	if len(secret) == 0 { 
+		return "", errors.New("Secret key cannot be empty.")
+	}
+	
+	encryptedBytes, err := process(
+			[]byte(rawString), 
+			[]byte(secret),
+		)
+
+	if err != nil {
+		return "", err
+	}	
+
+	return base64.StdEncoding.EncodeToString(encryptedBytes), nil
+}
+
+func Decipher(cipheredText, secret string) (string, error) {
+	if len(secret) == 0 {
+		return "", errors.New("Secret key cannot be empty.")
+	}
+	
+	cipheredBytes, err := base64.RawStdEncoding.DecodeString(
+		cipheredText,
+	)
+
+	if err != nil {
+		return "", err
+	}
+
+	decryptedBytes, err := process(
+		cipheredBytes,
+		[]byte(secret),
+	)
+
+	if err != nil { 
+		return "", err
+	}
+
+	return string(decryptedBytes), nil
+}
+
+func process(input, secret []byte) ([]byte, error) {
+	for i, b := range input {
+		input[i] = b ^ secret[i % len(secret)] // 0..len(secret)
+	}
+
+	return input, nil
+}
